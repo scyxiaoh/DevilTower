98bf3a9f7d916c74e07ab962fd964460f99d3609
Regina <Regina@192.168.2.67>
2018-01-13 16:48:23 -0500

    PauseScene

--- src/scene/GameScene.java
+++ src/scene/GameScene.java
@@ -59,9 +59,8 @@ public class GameScene extends Scene {
 
     public void handleInput() {
         if (KeyboardHandler.isPressed(Keys.E)) {
-        	//TODO
-            //SceneManager sM = SceneManager.getInstance();
-            //sM.setPaused(true);
+        	SceneManager sM = SceneManager.getInstance();
+        	sM.setPaused(true);
         }
         if (KeyboardHandler.isDown(Keys.UP)) {
             player.move(Direction.North);
